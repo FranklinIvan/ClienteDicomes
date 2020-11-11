6bc4ad22762ac1fc3c7af2f3b5ae989a8f45dec5
Franklin Alvarado <franklinalvarado0921@gmail.com>
2020-11-10 23:25:50 -0600

    mejorando el calendario de eventos...

--- views/bienvenido.php
+++ views/bienvenido.php
@@ -30,12 +30,13 @@ require('../views/sections/superior.php');
         },
 
         dateClick: function(info) {
+          $('#fecha').val(info.dateStr);
           $('#dayModal').modal();
           console.log(info);
-          /* calendar.addEvent({
+          calendar.addEvent({
             title:"Evento x",
             date:info.dateStr
-          }); */
+          });
 
           /* alert('Date: ' + info.dateStr);
           alert('Resource ID: ' + info.resource.id); */
@@ -81,6 +82,35 @@ require('../views/sections/superior.php');
       });
       calendar.setOption('locale', 'es');
       calendar.render();
+
+      $('#btnEnviar').click(function(){
+        recolectarDatosGUI("POST");
+      });
+
+      function recolectarDatosGUI(method){
+        
+        nuevoEvento={
+          solicitante:$('#nombre').val(),
+          fecha:$('#fecha').val(),
+          ubicacion:$('#ubicacion').val(),
+          hora_inicio:$('#horaInicio').val(),
+          hora_final:$('#horaFinal').val(),
+          tipo_servicio:$('#tipoServicio').val(),
+          tipo_evento:$('#tipoEvento').val(),
+          cantidad_personas:$('#cantidadPersonas').val(),
+          descripcion:$('#descripcion').val(),
+
+          '_method':method
+        }
+        console.log(nuevoEvento);
+      }
+
+      /* function EnviarInfo(accion,objEvento){
+        $.ajax({
+          type:"POST"
+        });
+      } */
+
     });
   </script>
 
@@ -100,60 +130,60 @@ require('../views/sections/superior.php');
         <div class="modal-body">
 
           <div class="form-group">
-            <label> <span class="font-weight-bold">De:</span> Fial </label>
+            <label> <span class="font-weight-bold">De:</span> <input type="hidden" name="nombre" id="nombre" value="Fraklooon loco"> Fial </label>
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Fecha del Evento:</label>
-            <input type="date" class="form-control font-italic" name="fecha" placeholder="fecha..." required>
+            <input type="date" class="form-control font-italic" name="fecha" id="fecha" readonly>
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Ubicación:</label>
-            <input type="text" class="form-control font-italic" name="ubicacion" placeholder="ubicación..." required>
+            <input type="text" class="form-control font-italic" name="ubicacion" id="ubicacion" placeholder="ubicación..." required>
           </div>
           <div class="form-group">
             <label> <span class="font-weight-bold">Hora Inicio:</span></label>
-            <input type="time" class="form-control font-italic" name="horaInicio" placeholder="hora inicial..." required>
+            <input type="time" class="form-control font-italic" name="horaInicio" id="horaInicio" placeholder="hora inicial..." required>
           </div>
           <div class="form-group">
             <label> <span class="font-weight-bold">Hora Final:</span></label>
-            <input type="time" class="form-control font-italic" name="horaFinal" placeholder="hora final..." required>
+            <input type="time" class="form-control font-italic" name="horaFinal" id="horaFinal" placeholder="hora final..." required>
           </div>
           <div class="form-group">
             <label> <span class="font-weight-bold">Tipo de Servicio:</span></label>
             <div class="input-group mb-3">
-              <select class="custom-select">
+              <select name="tipoServicio" id="tipoServicio" class="custom-select">
                 <!-- <option selected>Escoger...</option> -->
-                <option name="graduacion">Graduación</option>
-                <option name="congreso">Congreso</option>
-                <option name="seminario">Seminario</option>
-                <option name="presentacion">Presentación</option>
-                <option name="evento">Evento</option>
-                <option name="otro">Otro</option>
+                <option name="Graduación" id="Graduación">Graduación</option>
+                <option name="Congreso" id="Congreso">Congreso</option>
+                <option name="Seminario" id="Seminario">Seminario</option>
+                <option name="Presentación" id="Presentación">Presentación</option>
+                <option name="Evento" id="Evento">Evento</option>
+                <option name="Otro" id="Otro">Otro</option>
               </select>
             </div>
           </div>
           <div class="form-group">
             <label> <span class="font-weight-bold">Tipo de evento:</span></label>
             <div class="input-group mb-3">
-              <select class="custom-select">
-                <option name="publico">Público</option>
-                <option name="privado">Privado</option>
+              <select name="tipoEvento" id="tipoEvento" class="custom-select">
+                <option name="Público" id="Público">Público</option>
+                <option name="Privado" id="Privado">Privado</option>
               </select>
             </div>
           </div>
           <div class="form-group">
             <label> <span class="font-weight-bold">Cantidad de personas:</span></label>
-            <input type="number" min="1" class="form-control font-italic" name="cantidadPersonas" placeholder="cantidad..." required>
+            <input type="number" min="1" class="form-control font-italic" name="cantidadPersonas" id="cantidadPersonas" placeholder="cantidad..." required>
           </div>
           <div class="form-group">
             <label class="font-weight-bold">Descripción:</label><br>
-            <textarea name="descripcion" id="" cols="57" rows=5></textarea>
+            <textarea name="descripcion" id="descripcion" id="" cols="57" rows=5></textarea>
           </div>
 
         </div>
 
         <div class="modal-footer">
-          <button class="btn text-white" style="background-color: #0f9bd0;">Enviar</button>
+          <button class="btn text-white" name="btnEnviar" id="btnEnviar" style="background-color: #0f9bd0;">Enviar</button>
           <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Cerrar</button>
         </div>
       </div>
